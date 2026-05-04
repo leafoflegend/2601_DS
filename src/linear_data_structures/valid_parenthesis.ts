a1d0981c9a73dd3f8eaeeac8f5188e41fcd3f59c
Eliot Szwajkowski <eliotpszw@gmail.com>
2026-05-04 19:20:12 -0400

    add valid parenthesis problem

---
+++ src/linear_data_structures/valid_parenthesis.ts
@@ -0,0 +1,53 @@
+/*
+Given a string s containing any character, determine if it has a valid series of brackets/parenthesis: '(', ')', '{', '}', '[' and ']'. Determine if the input string is valid.
+
+An input string is valid if:
+
+    1. Open brackets must be closed by the same type of brackets.
+    2. Open brackets must be closed in the correct order.
+    3. Every close bracket has a corresponding open bracket of the same type.
+
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+
+Example 4:
+
+Input: s = "([])"
+Output: true
+
+Example 5:
+
+Input: s = "([)]"
+Output: false
+
+Example 6:
+
+Input: s = "i love cats (and dogs)"
+Output: true
+
+Example 7:
+
+Input: s = "racecars go [=> vroom!"
+Output: false
+
+Example 8:
+
+Input: s = ""
+Output: true
+*/
+
+const validParenthesis = (aString) => {
+    // TODO: Solve the problem!
+}
